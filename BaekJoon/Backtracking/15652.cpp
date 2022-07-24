6d3c24605b1b0dc4d7f7f57b4e881d7e6bc23daa
JHongsee <81742357+JHongsee@users.noreply.github.com>
2022-07-24 14:30:18 +0900

    Create 15652.cpp

---
+++ BaekJoon/Backtracking/15652.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+vector<int> v;
+vector<int> result;
+
+void outcome() {
+	for(auto i : result)
+		cout << i << " ";
+	cout << "\n";
+}
+
+void read() {
+	cin >> N >> M;
+	for(int i = 1; i <= N; i++) {
+		v.push_back(i);
+	}
+}
+
+void operating(int n, int cnt) {
+	if(cnt == M) {
+		outcome();
+		return;
+	}
+	for(int i = n; i < N; i++) {
+		result.push_back(v[i]);
+		operating(i, cnt+1);
+		result.pop_back();
+	}
+}
+
+int main() {
+	read();
+	operating(0, 0);
+	return 0;
+}
